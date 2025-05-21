30d487be72dc01b2d7ac999538080b1f8ee34363
Neville Muli <neville.muli@gmail.com>
2025-05-21 12:24:25 +0300

    adds relationship to orders and customers and creates order method

--- lib/customer.py
+++ lib/customer.py
@@ -15,4 +15,16 @@ class customer:
             raise TypeError("Name has to be a string")
         if not 3 <= len(name):
             raise  ValueError("Name has to be greater than or equal to three characters")
-        self._name = name
+        self._name = name 
+    
+    def orders(self):
+        return [order for order in Order.all if order.customer == self]
+    
+    def coffees(self):
+        return list({order.coffee for order in self.orders})
+
+    def create_order(self, cofee, price):
+        return Order(self.coffee, price) 
+    
+    @classmethod
+    

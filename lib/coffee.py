163e1aab1bef33caf6adb0d0e0d5fb8f30801fe8
Neville Muli <neville.muli@gmail.com>
2025-05-21 12:22:27 +0300

    finishes coffee class

--- lib/coffee.py
+++ lib/coffee.py
@@ -1,5 +1,7 @@
 class coffee:
 
+    all = []
+
     def __init__(self, name):
         self.name = name
 
@@ -14,3 +16,18 @@ class coffee:
         if not 1 <= len(name) <= 15:
             raise ValueError("Name must be between 1 to 15 characters")
         self._name = name
+    
+    def orders(self):
+        return [order for order in Order.all if order.coffee == self]
+    
+    def  customers(self):
+        return list({order.customer for order in self.orders()})
+    
+    def num_orders(self):
+        return len(self.orders())
+
+    def average_price(self):
+        orders = self.rders()
+        if not orders:
+            return 0
+        return sum(order.price for orders in orders) / len(orders)
